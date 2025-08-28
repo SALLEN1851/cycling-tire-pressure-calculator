f3fd5c0d3f691259aa9063e1d974b5b283da51e7
sallen <sallen1851@gmail.com>
2025-08-28 16:56:08 +0000

    update 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+// Restore SPA route after GitHub Pages 404 redirect
+const redirect = sessionStorage.getItem('redirect');
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, '', redirect);
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
